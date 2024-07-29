cc8aca23951a5c7a0ed98643485831dfe68f78aa
Igor <igorrafael_9@hotmail.com>
2024-07-29 20:24:03 -0300

    feat: create user model

---
+++ api-remember-it/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_remember_it.Models
+{
+    [Table("user")]
+    public class User
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        [Column("password")]
+        public string Password { get; set; }
+        [Column("birthday")]
+        public DateTime Birthday { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("profile_picture_url")]
+        public string? ProfilePictureUrl { get; set; }
+        [Column("phone_number")]
+        public string? PhoneNumber { get; set; }
+        [Column("is_active")]
+        public bool IsActive { get; set; }
+    }
+}
