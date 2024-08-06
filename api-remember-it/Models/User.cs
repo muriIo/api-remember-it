089bb62f1020dd1bf242003c1f05bf3f88797398
Igor <igorrafael_9@hotmail.com>
2024-08-06 19:14:13 -0300

    feat: adding password util and update user model

--- api-remember-it/Models/User.cs
+++ api-remember-it/Models/User.cs
@@ -1,20 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api_remember_it.Models
 {
     [Table("user")]
+    [Index(nameof(Id), nameof(Email), IsUnique = true)]
     public class User
     {
         [Column("id")]
         public int Id { get; set; }
-        [Column("email")]
-        public string Email { get; set; }
-        [Column("password")]
-        public string Password { get; set; }
-        [Column("birthday")]
-        public DateTime Birthday { get; set; }
         [Column("name")]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [Column("email")]
+        public string Email { get; set; } = string.Empty;
+        [Column("password_hash")]
+        public string PasswordHash { get; set; } = string.Empty;
+        [Column("password_salt")]
+        public string PasswordSalt { get; set; } = string.Empty;
+        [Column("birthday")]
+        public DateTimeOffset Birthday { get; set; } = DateTimeOffset.Now;
         [Column("profile_picture_url")]
         public string? ProfilePictureUrl { get; set; }
         [Column("phone_number")]
---
+++ api-remember-it/Utils/PasswordUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace api_remember_it.Utils
+{
+    public class PasswordUtil
+    {
+        public static void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = Convert.ToHexString(hmac.Key);
+                passwordHash = Convert.ToHexString(hmac.ComputeHash(Encoding.UTF8.GetBytes(password)));
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, string passwordHash, string passwordSalt)
+        {
+            byte[] passwordSaltByte = Convert.FromHexString(passwordSalt);
+            byte[] passwordHashByte = Convert.FromHexString(passwordHash);
+            using (var hmac = new HMACSHA512(passwordSaltByte))
+            {
+                var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computeHash.SequenceEqual(passwordHashByte);
+            }
+        }
+    }
+}
